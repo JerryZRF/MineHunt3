123a1154c0830f238a66e9d75099c5c9b488903a
JerryZRF <858479588@qq.com>
2021-08-23 20:23:39 +0800

    第一次提交

--- src/main/java/net/mcbbs/jerryzrf/minehunt/util/MusicPlayer.java
+++ src/main/java/net/mcbbs/jerryzrf/minehunt/util/MusicPlayer.java
@@ -10,22 +10,6 @@ import org.bukkit.scheduler.BukkitRunnable;
 public class MusicPlayer {
     @SneakyThrows
     public void playEnding() {
-//        本来设计放红石音乐 想想算了
-//        if(Bukkit.getPluginManager().isPluginEnabled("NoteBlockAPI")) {
-//          //  Files.copy(MineHunt.getInstance().getResource("newNBS.nbs"),new File(MineHunt.getInstance().getDataFolder(),"nbs.nbs").toPath());
-//            Song song = NBSDecoder.parse(MineHunt.getInstance().getResource("newNBS.nbs"));
-//            if(song == null){
-//                MineHunt.getInstance().getLogger().warning("Cannot play NBS song cause it is null.");
-//                return;
-//            }
-//            RadioSongPlayer rsp = new RadioSongPlayer(song);
-//            Bukkit.getOnlinePlayers().forEach(rsp::addPlayer);
-//            rsp.setPlaying(true);
-//        }
-//        Bukkit.getOnlinePlayers().forEach(p->{
-//            if(p.isDead()){p.spigot().respawn();}
-//        });
-        
         new BukkitRunnable() {
             @Override
             public void run() {
